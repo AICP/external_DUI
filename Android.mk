eaa0cc8902e61086042eb6878aa97364996fcf53
Alex <alex@dirtyunicorns.com>
2018-06-27 07:43:12 -0500

    Modify paths to avoid issues inheriting it into the fwb

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_SRC_FILES := $(call all-java-files-under, src/com/android/internal)
 
 LOCAL_MODULE_TAGS := optional
 
